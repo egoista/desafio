a0b7b5cf95f37db8dda0d0bfcdacee26f6e55cd7
Felipe de Almeida <felipe.silveira@foxbit.com.br>
2020-08-10 01:29:04 -0300

    add v1 namespaec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :stock_items
-  resources :products
-  resources :stores
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1 do
+    resources :stock_items
+    resources :products
+    resources :stores
+  end
 end
