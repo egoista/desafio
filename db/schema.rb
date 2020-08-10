f06bfc95cdf1467d495db6c13053c5e455fc1ab7
Felipe de Almeida <felipe.silveira@foxbit.com.br>
2020-08-10 01:25:36 -0300

    set volume bind in docker-compose.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_001843) do
+ActiveRecord::Schema.define(version: 2020_08_10_040518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "stock_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "product_id", null: false
+    t.bigint "store_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_stock_items_on_product_id"
+    t.index ["store_id"], name: "index_stock_items_on_store_id"
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "name"
@@ -22,4 +39,6 @@ ActiveRecord::Schema.define(version: 2020_08_10_001843) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "stock_items", "products"
+  add_foreign_key "stock_items", "stores"
 end
