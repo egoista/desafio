5da2dce95a0b6034d5ef585d9c33d68abd4919b4
Felipe de Almeida <felipe.silveira@foxbit.com.br>
2020-08-11 10:44:55 -0300

    add stock items validations and tests

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -1,4 +1,8 @@
 class StockItem < ApplicationRecord
   belongs_to :product
   belongs_to :store
+
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates_presence_of :quantity
+  validates :product_id, uniqueness: { scope: :store_id }
 end
--- spec/models/stock_item_spec.rb
+++ spec/models/stock_item_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe StockItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    describe 'uniqueness of product and store' do
+      context 'with same product and store' do
+        let(:store) { create(:store) }
+        let(:product) { create(:product) }
+        it 'invalid entity' do
+          StockItem.create(product: product, store: store, quantity: 1)
+          invalid_item = StockItem.new(product: product, store: store, quantity: 1)
+          expect(invalid_item).to_not be_valid
+        end
+      end
+
+      context 'with different product' do
+        let(:store) { create(:store) }
+        let(:product_1) { create(:product) }
+        let(:product_2) { create(:product) }
+        it 'valid entity' do
+          StockItem.create(product: product_1, store: store, quantity: 1)
+          invalid_item = StockItem.new(product: product_2, store: store, quantity: 1)
+          expect(invalid_item).to be_valid
+        end
+      end
+
+      context 'with different store' do
+        let(:store_1) { create(:store) }
+        let(:store_2) { create(:store) }
+        let(:product) { create(:product) }
+        it 'valid entity' do
+          StockItem.create(product: product, store: store_1, quantity: 1)
+          invalid_item = StockItem.new(product: product, store: store_2, quantity: 1)
+          expect(invalid_item).to be_valid
+        end
+      end
+    end
+
+    let(:store) { create(:store) }
+    let(:product) { create(:product) }
+
+    describe 'presensence of quantity' do
+      it 'is invalid without name' do
+        invalid_item = described_class.new(product: product, store: store)
+
+        expect(invalid_item).to_not be_valid
+        expect(invalid_item.errors.details).to eq({ quantity: [{:error=>:not_a_number, :value=>nil}, {:error=>:blank}] })
+      end
+    end
+
+    describe 'presensence of product' do
+      it 'is invalid without product' do
+        invalid_item = described_class.new(quantity: 1, store: store)
+
+        expect(invalid_item).to_not be_valid
+        expect(invalid_item.errors.details).to eq({ product: [{ error: :blank }] })
+      end
+    end
+
+    describe 'presensence of store' do
+      it 'is invalid without store' do
+        invalid_item = described_class.new(quantity: 1, product: product)
+
+        expect(invalid_item).to_not be_valid
+        expect(invalid_item.errors.details).to eq({ store: [{ error: :blank }] })
+      end
+    end
+  end
 end
