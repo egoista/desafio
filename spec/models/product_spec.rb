839e9d60ab557e73128c3f5654f049c02e503645
Felipe de Almeida <felipe.silveira@foxbit.com.br>
2020-08-11 10:43:08 -0300

    add product model spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    describe 'presensence of name' do
+      it 'is invalid without name' do
+        invalid_product = described_class.new(price: 1)
+
+        expect(invalid_product).to_not be_valid
+        expect(invalid_product.errors.details).to eq({ name: [{ error: :blank }] })
+      end
+    end
+    describe 'presensence of price' do
+      it 'is invalid without name' do
+        invalid_product = described_class.new(name: 'foo')
+
+        expect(invalid_product).to_not be_valid
+        expect(invalid_product.errors.details).to eq({ price: [{ error: :blank }] })
+      end
+    end
+  end
 end
