d2a803576064e49b8929d0643c8616aa1a78cb3f
Felipe de Almeida <felipe.silveira@foxbit.com.br>
2020-08-11 10:44:32 -0300

    add store validations and tests

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,5 @@
 class Store < ApplicationRecord
+  has_many :stock_items
+
+  validates_presence_of :name, :address
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    describe 'presensence of name' do
+      it 'is invalid without name' do
+        invalid_store = described_class.new(address: 'adrress')
+
+        expect(invalid_store).to_not be_valid
+        expect(invalid_store.errors.details).to eq({ name: [{ error: :blank }] })
+      end
+    end
+
+    describe 'presensence of address' do
+      it 'is invalid without name' do
+        invalid_store = described_class.new(name: 'name')
+
+        expect(invalid_store).to_not be_valid
+        expect(invalid_store.errors.details).to eq({ address: [{ error: :blank }] })
+      end
+    end
+  end
 end
